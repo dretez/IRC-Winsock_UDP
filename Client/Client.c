657bb3ed19c6a15558fa739796c6cfac59062799
André Maltez <105381869+dretez@users.noreply.github.com>
2024-09-27 18:04:06 +0100

    Removed most comments

--- Client/Client.c
+++ Client/Client.c
@@ -50,13 +50,6 @@ int main(int argc, char* argv[])
 	* No fundo estamos a indicar que a nossa aplicação pretende usar
 	* Windows Sockets e será retornado um código de erro caso não
 	* seja possível.
-	*
-	* NOTA: esta função não está no formulário que terão acesso
-	* no dia do teste e por isso não será alvo de avaliação nos testes
-	* práticos da minha turma.
-	*
-	* NOTA2: falo apenas por mim, não garanto que não apareça em testes
-	* de outros professores!
 	*/
 	iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
 	if (iResult != 0) {
@@ -67,37 +60,12 @@ int main(int argc, char* argv[])
 
 	/*=============== CRIA SOCKET PARA ENVIO/RECEPCAO DE DATAGRAMAS ==============*/
 
-	/*
-	* Nas linhas seguintes estamos a criar o Socket que será usado na comunicação
-	* entre aplicações.
-	*/
 	sockfd = socket(PF_INET, SOCK_DGRAM, 0);
 	if (sockfd == INVALID_SOCKET)
 		Abort("Impossibilidade de criar socket");
 
 	/*================= PREENCHE ENDERECO DO SERVIDOR ====================*/
 
-	/*
-	* Nas linhas seguintes estamos a preencher o endereço do servidor. Lembrem-se
-	* que o protocolo UDP é como uma carta de correio e por isso têm sempre que
-	* especificar o endereço IP e porto do destinatário, ou seja, para onde querem
-	* enviar o datagrama.
-	* Notem que existem funções "auxiliares" que precisamos de usar para especificar
-	* o IP e porto. Estas funções permitem converter caracteres/inteiros/... para algo
-	* que seja reconhecido pelos sockets (mais explicação no pdf disponibilizado)
-	* 
-	* NOTA: na linha 101 estamos basicamente a limpar a estrutura serv_addr, ou seja,
-	*       estamos a zerar todos os bytes dessa estrutura.
-	*		Isto é importante para evitarmos ter lixo nas propriedades que irão
-	*		guardar o endereço IP e porto do servidor.
-	* 
-	* NOTA2: como isto é puramente C (não é algo introduzido pelos sockets), não será
-	*		 alvo de avaliação nos testes práticos da minha turma. No entanto, devem
-	*		 ter uma noção do que a função memset faz.
-	* 
-	* NOTA3: mais uma vez falo apenas por mim, não garanto que não apareça em testes
-	* de outros professores!
-	*/
 	memset((char*)&serv_addr, 0, sizeof(serv_addr)); /*Coloca a zero todos os bytes*/
 	serv_addr.sin_family = AF_INET; /*Address Family: Internet*/
 	serv_addr.sin_addr.s_addr = inet_addr(SERV_HOST_ADDR); /*IP no formato "dotted decimal" => 32 bits*/
@@ -105,21 +73,6 @@ int main(int argc, char* argv[])
 
 	/*====================== ENVIA MENSAGEM AO SERVIDOR ==================*/
 
-	/*
-	* Nas linhas seguintes vamos enviar a mensagem para o servidor. Para isso temos que usar
-	* a função sendto(...) e temos que especificar todos os parametros.
-	* 
-	* NOTA: vejam a especificação de cada parametro no pdf que disponibilizei no Github.
-	* 
-	* NOTA2: tomem especial atenção para o tipo de parametros que devem passar, se a função
-	*		 sendto() estiver à espera de um ponteiro então têm que passar a referência da
-	*		 variável (usando o caracter &).
-	*		 Muita gente falha isto nos testes práticos.
-	* 
-	* NOTA3: tomem atenção ao sizeof() vs strlen()
-	*		 Mais uma vez, olhem para o pdf pois está lá a diferença entre eles.
-	*		 Se mesmo assim tiverem dúvidas podem sempre entrar em contacto comigo.
-	*/
 	msg_len = strlen(argv[1]);
 
 	if (sendto(sockfd, argv[1], msg_len, 0, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == SOCKET_ERROR)
@@ -131,20 +84,6 @@ int main(int argc, char* argv[])
 
 	/*========================= RECEBER MENSAGEM ===========================*/
 
-	/*
-	* Nas linhas seguintes vamos receber a resposta do servidor e apresentá-la na consola.
-	* 
-	* NOTA: notem que os últimos parametros da função recvfrom() estão a NULL.
-	*		Para a resolução do exercício 2 estão bem assim, no entanto, isto terá que
-	*		ser alterado para os exercícios seguintes.
-	*		Irei explicar melhor estes parametros na função recvfrom() do lado do servidor.
-	*		E, mais uma vez, têm a explicação no pdf que disponibilizei.
-	* 
-	* NOTA2: MUITA ATENÇÃO para a linha 151!!!
-	*		 Nesta linha estamos a terminar a cadeia de caracteres para podermos apresentar
-	*		 corretamente no printf. A explicação está no pdf e irei explicar melhor nas
-	*		 minhas aulas práticas (é mais facil explicar dando um exemplo prático)
-	*/
 	nbytes = recvfrom(sockfd, buffer, sizeof(buffer), 0, NULL, NULL);
 
 	if (nbytes == SOCKET_ERROR)
@@ -156,10 +95,6 @@ int main(int argc, char* argv[])
 
 	/*========================= FECHA O SOCKET ===========================*/
 
-	/*
-	* Na linha seguinta vamos fechar o socket. Isto pode parecer algo insignificante mas não o é.
-	* Temos sempre que libertar recursos que já não iremos utilizar.
-	*/
 	closesocket(sockfd);
 
 	printf("\n");
--- Server/Server.c
+++ Server/Server.c
@@ -34,13 +34,6 @@ int main(int argc, char* argv[])
 	* No fundo estamos a indicar que a nossa aplicação pretende usar
 	* Windows Sockets e será retornado um código de erro caso não
 	* seja possível.
-	*
-	* NOTA: esta função não está no formulário que terão acesso
-	* no dia do teste e por isso não será alvo de avaliação nos testes
-	* práticos da minha turma.
-	*
-	* NOTA2: falo apenas por mim, não garanto que não apareça em testes
-	* de outros professores!
 	*/
 	iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
 	if (iResult != 0) {
@@ -51,10 +44,6 @@ int main(int argc, char* argv[])
 
 	/*============ CRIA O SOCKET PARA RECEPCAO/ENVIO DE DATAGRAMAS UDP ============*/
 
-	/*
-	* Nas linhas seguintes estamos a criar o Socket que será usado na comunicação
-	* entre aplicações.
-	*/
 	if ((sockfd = socket(PF_INET, SOCK_DGRAM, 0)) == INVALID_SOCKET)
 		Abort("Impossibilidade de abrir socket");
 
@@ -66,16 +55,6 @@ int main(int argc, char* argv[])
 	* sin_family
 	* sin_addr.s_addr
 	* sin_port
-	* 
-	* NOTA: notem que a linha 82, onde especificamos o IP, é diferente do que fazemos
-	*		do lado do cliente!
-	*		Isto porque, do lado do cliente estamos a especificar qual o IP do servidor
-	*		a quem queremos enviar um datagram, enquanto que, do lado do servidor,
-	*		estamos a especificar que pretendemos receber datagramas de qualquer
-	*		interface de rede (daí usarmos a constante INADDR_ANY).
-	* 
-	* NOTA2: tal como no cliente, aqui também temos que usar funções "auxiliares" para
-	*		 converter o IP e porto para algo reconhecido pelos sockets.
 	*/
 	memset((char*)&serv_addr, 0, sizeof(serv_addr));
 	serv_addr.sin_family = AF_INET; /*Address Family: Internet*/
@@ -85,10 +64,9 @@ int main(int argc, char* argv[])
 	/*
 	* Na linha seguinte vamos então efetivamente associar o socket ao porto pretendido.
 	* Para isso usamos a função bind() e é onde será feita a verificação se o porto
-	* já está ou não a ser utilizado. Como sabem, só podemos ter uma aplicação a usar
-	* o mesmo porto, logo, se o porto já estiver a ser usado então a aplicação será
-	* terminada e apresentamos a mensagem de erro, neste caso: "Impossibilidade de
-	* registar-se para escuta"
+	* já está ou não a ser utilizado. Se o porto já estiver a ser usado então a
+	* aplicação será terminada e apresentamos a mensagem de erro, neste caso: 
+	* "Impossibilidade de registar-se para escuta"
 	*/
 	if (bind(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == SOCKET_ERROR)
 		Abort("Impossibilidade de registar-se para escuta");
@@ -100,40 +78,12 @@ int main(int argc, char* argv[])
 	* Nas linhas seguintes vamos atender os clientes.
 	* Estamos a usar um ciclo "infinito" (ciclo while) pois o objetivo do servidor
 	* é estar sempre a atender clientes.
-	* 
-	* NOTA: não tem necessariamente que ser sempre assim. Podemos querer ter um servidor
-	*		que só atende X numero de clientes.
 	*/
 
 	while (1) {
 
 		fprintf(stderr, "<SER1>Esperando datagrama...\n");
 
-		/*
-		* Nas linhas seguintes vamos receber a mensagem do cliente e apresentá-la na consola.
-		*
-		* NOTA: notem que os últimos parametros da função recvfrom() já não estão a NULL.
-		*		Isto porque, para a resolução do exercício 2, precisamos de saber de onde veio
-		*		a mensagem para a podermos enviar de volta.
-		* 
-		*		Para sabermos de onde a mensagem veio vamos ter ajuda da função recvfrom().
-		*		Por isso, criamos uma variável do tipo struct sockaddr_in e metemo-la nos
-		*		dois últimos parametros da função recevfrom(). Será a própria função recevfrom()
-		*		que irá preencher essa estrutura com os dados (IP e porto) de onde o datagrama
-		*		foi recebido, ou seja, passamos uma estrutura vazia e a função recevfrom() trata
-		*		de a preencher com os dados que queremos!
-		* 
-		*		Pensem nisto como um envelope:
-		*		1º criamos um envelope vazio
-		*		2º colocamos o envelope vazio na função recevfrom()
-		*		3º a função recevfrom() preenche o envelope com os dados que queremos
-		*		4º depois só temos que usar o envelope pois os dados (IP e porto) já lá estão
-		*
-		* NOTA2: MUITA ATENÇÃO para a linha 143!!!
-		*		 Nesta linha estamos a terminar a cadeia de caracteres para podermos apresentar
-		*		 corretamente no printf. A explicação está no pdf e irei explicar melhor nas
-		*		 minhas aulas práticas (é mais facil explicar dando um exemplo prático)
-		*/
 		length_addr = sizeof(cli_addr);
 		nbytes = recvfrom(sockfd, buffer, sizeof(buffer), 0, (struct sockaddr*)&cli_addr, &length_addr);
 
@@ -147,17 +97,6 @@ int main(int argc, char* argv[])
 
 		/*====================== ENVIA MENSAGEM AO CLIENTE ==================*/
 
-		/*
-		* Na linha seguinte vamos usar a função sendto() para enviar a resposta ao cliente.
-		* Lembrem-se que sempre que queremos enviar um datagrama temos que especificar um
-		* endereço IP e porto de destino. 
-		* E onde está o IP e porto do cliente? Está na estrutura que passamos para a função
-		* recevfrom e que esta preencheu por nós. Portanto, agora só temos que a usar na função
-		* sendto()... fácil não? :) 
-		* 
-		* NOTA: percebo que isto não seja tão trivial e fácil de entender por isso estou sempre
-		*		disponível para vos ajudar!
-		*/
 		if (sendto(sockfd, buffer, strlen(buffer), 0, (struct sockaddr*)&cli_addr, sizeof(cli_addr)) == SOCKET_ERROR)
 			Abort("O subsistema de comunicacao nao conseguiu aceitar o datagrama");
 
